43a5aa34b9a5a8cc9f78e48e28c353e9e76fcad3
divyam2080 <divyam.gupta_cs18@gla.ac.in>
2020-03-31 20:08:59 +0530

    importing the required  feilds

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,12 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.Student;
+import problem3.myqueue.MyPriorityQueue;
+
+import java.util.Scanner;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+
 }
