315787ac8acbef8fe1e310f58e004c4fc252f79b
divyam2080 <divyam.gupta_cs18@gla.ac.in>
2020-03-31 20:48:14 +0530

    binary search tree created

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,22 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
+import problem1.mybst.MyBinarySearchTree;
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(35);
+        binarySearchTree.insert(90);
+        binarySearchTree.insert(15);
+        binarySearchTree.insert(45);
+        binarySearchTree.insert(33);
+
+        System.out.println("Left children is :");
+        binarySearchTree.traverse();
+        System.out.println("No. of parents not having left child :");
+        System.out.println(binarySearchTree.getCountRight());
+    }
 }
